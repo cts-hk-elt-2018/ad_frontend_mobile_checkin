8bcddee6359d74e687281651c79e67f90522ff37
ckho <me@ckho.info>
2018-11-10 23:57:00 +0800

    Route setting

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Success from "./views/Success.vue";
+import Err from "./views/Error.vue";
 
 Vue.use(Router);
 
@@ -12,13 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/success",
+      name: "success",
+      component: Success
+    },
+    {
+      path: "/error",
+      name: "error",
+      component: Err
     }
   ]
 });
