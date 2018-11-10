7695c174129cb6de82e67271e87355687fdc36fa
ckho <me@ckho.info>
2018-11-10 23:57:24 +0800

    Return Message

---
+++ src/views/Error.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="success">
+    <div class="main">
+      <div class="container">
+        <p id="error">Please contact our flight attendant.</p>
+      </div>
+    </div>
+    <div class="cover cover3">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+};
+</script>
+
+<style lang="scss">
+.cover3 {
+  width: 100%;
+  height: 100vh;
+  background: #0033a0;
+  position: absolute;
+  z-index: 0;
+  bottom: 0px;
+}
+#error {
+  margin: 0 auto;
+  width: 95%;
+  height: 100%;
+  border: none;
+  background: transparent;
+  font-size: 3.8vh;
+  font-weight: bold;
+  box-sizing: border-box;
+  font-family: 'Montserrat';
+  padding-top: 8%;
+  padding-bottom: 9%;
+  outline: none;
+  appearance: unset !important;
+  -moz-appearance: unset !important;
+  -webkit-appearance: unset !important;
+  -o-appearance: unset !important;
+  -ms-appearance: unset !important;
+  -webkit-appearance: none;
+  transition: 0.3s ease;
+  -moz-transition: 0.3s ease;
+  -webkit-transition: 0.3s ease;
+  -o-transition: 0.3s ease;
+  -ms-transition: 0.3s ease;
+}
+</style>
